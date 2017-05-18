d30e258cd57b1af27f20087c7a88a615e377c407
Nishant Mandapaty <nishantmandapaty@gmail.com>
2017-05-17 23:14:11 -0700

    Fix json error

--- packages/rocketchat-twilio/twilio.js
+++ packages/rocketchat-twilio/twilio.js
@@ -1,6 +1,6 @@
 Meteor.methods({
 	setupTwilio(user,room){
-		HTTP.call(‘GET’, 'http://7993b0a5.ngrok.io/token', function(error, res) {
+		HTTP.call('GET', 'http://7993b0a5.ngrok.io/token', function(error, res) {
 		  var token;
 		  if (error) {
 		    console.log(error);
