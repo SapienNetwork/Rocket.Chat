5dfa13e0d60d7270f9199ebb0103eda11db596fa
Robert Giometti <Rgiometti@berkeley.edu>
2017-06-08 12:33:17 -0600

    fix bug in exitVoiceChannel

--- client/methods/exitVoiceChannel.js
+++ client/methods/exitVoiceChannel.js
@@ -1,6 +1,14 @@
 Meteor.methods({
 	exitVoiceChannel() {
-		console.log('exitVoice');
+		//console.log('exitVoice');
+		
+		if(FlowRouter.getRouteName() === 'voice'){
+			if(Session.get('openedRoom')){
+				FlowRouter.goToRoomById(Session.get('openedRoom'));
+			}else{
+				FlowRouter.go('home');
+			}
+		}
 		Session.set('openedVoiceChannel', null);
 		Session.set('mostRecentRoomType', 'c');
 		Twilio.Device.disconnectAll();
