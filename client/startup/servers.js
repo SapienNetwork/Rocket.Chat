5108f54c11caf01be4b763e57115b65ea2b9227f
maabed <veto44@gmail.com>
2018-03-12 12:33:21 +0200

    server observe on client side, not ready

---
+++ client/startup/servers.js
@@ -0,0 +1,15 @@
+
+// XXX
+// Meteor.startup(() => {
+// 	Servers.find().observe({
+// 		added(data) {
+// 			Session.set('Server', data);
+// 		},
+// 		changed(data) {
+// 			Session.set('Server', data);
+// 		},
+// 		removed() {
+// 			Session.set('Server', null);
+// 		}
+// 	});
+// });
